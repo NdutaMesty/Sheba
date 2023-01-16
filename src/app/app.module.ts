0e5ba23c7aceb018690dcb7a450fdf751ec70ee1
kingsley-people-box <kingsley@peoplebox.ai>
2023-01-16 11:35:01 +0100

    setup template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LandingComponent,
+    CheckoutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
