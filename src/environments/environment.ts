7ba09dd662f82a325fed8e6103e84806e28e9c43
Ruth Nduta <ndutaruthmuriithi@gmail.com>
2023-01-24 21:47:06 +0000

    final edits

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+  apiKey: "AIzaSyDkrDXnIPJXpa7ylzsebxVl1bPa6xHktC8",
+  authDomain: "sheba-79f22.firebaseapp.com",
+  projectId: "sheba-79f22",
+  storageBucket: "sheba-79f22.appspot.com",
+  messagingSenderId: "270011379280",
+  appId: "1:270011379280:web:2cdd3cd0abf37973c5eb55"
+}
+};
